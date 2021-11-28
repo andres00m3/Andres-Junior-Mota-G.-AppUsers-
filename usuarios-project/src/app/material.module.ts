722213f075c0eaa02f61fe1c739986ee4b2d3f59
andres junior <andresjunior67@hotmail.com>
2021-11-27 22:58:03 -0400

    commit

--- usuarios-project/src/app/material.module.ts
+++ usuarios-project/src/app/material.module.ts
@@ -2,16 +2,18 @@ import {NgModule} from '@angular/core';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import {MatTableModule} from '@angular/material/table';
-
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 
 @NgModule({
 imports:[MatToolbarModule,
   MatIconModule,
-  MatTableModule],
+  MatTableModule,
+  MatFormFieldModule],
 exports:[MatToolbarModule,
   MatIconModule,
-  MatTableModule]
+  MatTableModule,
+  MatFormFieldModule]
 })
 
 export class MaterialModule{};
